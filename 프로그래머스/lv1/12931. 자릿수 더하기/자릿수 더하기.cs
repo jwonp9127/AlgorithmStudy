7d2a00286d1ee32438a8fa67cf40ce6d0c0001ae
jwonp <120299073+jwonp9127@users.noreply.github.com>
2023-08-24 10:36:49 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.16 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true){
+            answer += n%10;
+            n /= 10;
+            if(n<=0) break;
+        }
+        return answer;
+    }
+}

754abe606a32a3509f8a1853aaa1fa54b9b60db4
jwonp <120299073+jwonp9127@users.noreply.github.com>
2023-10-17 09:59:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.60 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public string solution(string s) {
+        string[] s_a = s.Split(' ');
+        string answer = "";
+        for(int i = 0; i < s_a.Length; i++){
+            if(s_a[i].Equals("")){
+                if(i != s_a.Length - 1) answer += " ";
+                continue;
+            }
+            answer += s_a[i].Substring(0, 1).ToUpper();
+            answer += s_a[i].Substring(1).ToLower();
+            if(i != s_a.Length - 1) answer += " ";
+        }
+        return answer;
+    }
+}
